233b2e29085f894952f50d4118f2777849fd0462
Atinder-Pal <atty.pal46@gmail.com>
2020-10-13 17:28:01 -0600

    added alternative answers for query1

--- sql_query_sheet.sql
+++ sql_query_sheet.sql
@@ -2,4 +2,15 @@
 SELECT CONCAT(contact.FirstName,' ', contact.LastName)AS EmployeeName
 FROM `contact` 
 WHERE contact.ContactID IN(SELECT employee.ContactID FROM `employee` WHERE VacationHours = (SELECT MAX(VacationHours) FROM employee));
--- ----------------------------
+--Query1 Alternative --------------------------------
+SELECT CONCAT(contact.FirstName,' ', contact.LastName)AS EmployeeName
+FROM employee
+INNER JOIN contact USING (ContactID)
+WHERE contact.ContactID IN(SELECT employee.ContactID FROM `employee` WHERE VacationHours = (SELECT MAX(VacationHours) FROM employee));
+-- ----------------------------------------------------
+--Query1 Alternative --------------------------------
+SELECT CONCAT(contact.FirstName,' ', contact.LastName)AS EmployeeName
+FROM contact
+INNER JOIN employee USING (ContactID)
+WHERE contact.ContactID IN(SELECT employee.ContactID FROM `employee` WHERE VacationHours = (SELECT MAX(VacationHours) FROM employee));
+-- ----------------------------------------------------
